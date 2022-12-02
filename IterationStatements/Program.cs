d88888d56db2a51e2fdd65f9cbf226e6d95d1514
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2022-12-01 18:44:18 -0700

    second push

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -40,13 +40,14 @@ namespace IterationStatements
             }
         }
         //Write a method to check whether a given number is even or odd
-        // 
+        // I was able to get this one without help :)
         public static string OddBall(int num)
         {
             return num % 2 == 0 ? "Even" : "Odd";
         }
 
         //Write a method to check whether a given number is positive or negative
+        // Completed this one without help, too!
         public static void PosOrNeg()
         {
             int n;
@@ -66,6 +67,11 @@ namespace IterationStatements
                 Console.WriteLine(n + " is a negative number");
             }
         }
+        //Write a method to read the age of a candidate and determine whether they can vote.
+        //Hint: Use Parse or the safer TryParse() for an extra challenge
+        //Parse()
+        //TryParse()
+        // I needed help all the way through on this one
         public static void CanIVote()
         {
             Console.WriteLine("How old are you? Enter your age below.");
@@ -81,27 +87,50 @@ namespace IterationStatements
             }
         }
 
-        //Write a method to read the age of a candidate and determine whether they can vote.
-        //Hint: Use Parse or the safer TryParse() for an extra challenge
-        //Parse()
-        //TryParse()
+       
 
         //Heatin Up Section:
         //Write a method to check if an integer(from the user) is in the range -10 to 10
+        // I was able to figure this one out without help
+        public static void OnTheRange()
+        {
+            Console.WriteLine("Is your number between 10 and -10?");
+            Console.WriteLine("Enter a number: ");
+            var result = int.Parse(Console.ReadLine());
 
+            if (result >= -10 && result <= 10)
+            {
+                Console.WriteLine("This number is within range");
+            }
+            else
+            {
+                Console.WriteLine("This number is not in range.");
+            }
+        }
         //Write a method to display the multiplication table(from 1 to 12) of a given integer
-
+        // The firt time I ran this code it was not stopping at 12.
+        // Went to lab and go some help to get through
+        public static void MultiPass()
+        {
+            Console.WriteLine("Enter a number to see the multiplication table");
+            int numb = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{i} * {numb} = {numb * i}");
+            }
+        }
 
         //Call the methods to test them in the Main method below
         public static void Main(string[] args)
         {
-            
+
             ManyNums();
             TimesThree();
             AreEqual(9, 19);
             OddBall(76);
             PosOrNeg();
             CanIVote();
+            MultiPass();
         }
 
 
